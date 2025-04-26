6527938895bf4284c7f35a33c52718139398325b
Yousrbekri <yousbk712@gmail.com>
2025-04-26 15:54:16 +0000

    fusionner classify et detect

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from models.mobilenet import classify_image as mobilenet_classify
-from models.yolov9 import detect_corrosion as yolov9_detect
-from schemas import ClassificationResponse, DetectionResponse
+from models.mobilenet import classify_image
+from models.yolov9 import detect_corrosion
+from schemas import UnifiedResponse
 import logging
-
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -20,33 +19,29 @@ app.add_middleware(
 )
 
 
-@app.post("/classify", response_model=ClassificationResponse)
-async def classify(file: UploadFile = File(...)):
+@app.post("/analyze", response_model=UnifiedResponse)
+async def analyze_image(file: UploadFile = File(...)):
     try:
         if not file.content_type.endswith((".png", ".jpg", ".jpeg", ".PNG", ".JPEG", ".JPG")):
             raise HTTPException(status_code=400, detail="An image file is required.")
 
         image_bytes = await file.read()
-        result = mobilenet_classify(image_bytes)
-        return result
+
+        classification_result = classify_image(image_bytes)
+
+        # yolo
+        detection_result = None
+        if classification_result["class_name"] == "corrosion" and classification_result["confidence"] > 0.5:
+            detection_result = detect_corrosion(image_bytes)
+
+        return {
+            "classification": classification_result,
+            "detection": detection_result
+        }
+
     except Exception as e:
-        logger.error(f"Erreur classification: {e}")
-        raise HTTPException(status_code=500, detail="Internal server Erreur.")
-
-
-@app.post("/detect", response_model=DetectionResponse)
-async def detect(file: UploadFile = File(...)):
-    try:
-        if not file.content_type.endswith((".png", ".jpg", ".jpeg", ".PNG", ".JPEG", ".JPG")):
-            raise HTTPException(status_code=400, detail="An image file is required.")
-
-        image_bytes = await file.read()
-        result = yolov9_detect(image_bytes)
-        return result
-    except Exception as e:
-        logger.error(f"Detection erreur: {e}")
-        raise HTTPException(status_code=500, detail="Internal server Erreur.")
-
+        logger.error(f"Error : {e}")
+        raise HTTPException(status_code=500, detail="Internal server Error.")
 
 @app.get("/health")
 def health_check():
--- app/schemas.py
+++ app/schemas.py
@@ -1,11 +1,14 @@
 from pydantic import BaseModel
 
-class ClassificationResponse(BaseModel):
+class ClassificationResult(BaseModel):
     class_name: str
     confidence: float
 
-class DetectionResponse(BaseModel):
-    bboxes: list[list[float]]  # Format: [xmin, ymin, xmax, ymax]
+class DetectionResult(BaseModel):
+    bboxes: list[list[float]]  # [xmin, ymin, xmax, ymax]
     scores: list[float]
     labels: list[str]
 
+class UnifiedResponse(BaseModel):
+    classification: ClassificationResult
+    detection: DetectionResult
